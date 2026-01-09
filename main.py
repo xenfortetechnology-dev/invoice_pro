9e9087922d58927bc025867d8862b88d6d5cafb2
arav124 <aravindmurugesan0803@gmail.com>
2026-01-09 16:21:05 +0530

    main.py file fixed

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-import os
-import threading
-import webview
+# import os
+# import threading
+
+# from app import app
+
+# def new_func():
+#     return True
+
+# app.debug = new_func()
+
+# def run_flask():
+#     app.run(
+#         host='127.0.0.1',
+#         port=5004,
+#         debug=False,
+#         use_reloader=False
+#     )
+
+# def on_closed():
+#     os._exit(0) 
+
+# if __name__ == '__main__':
+#     flask_thread = threading.Thread(target=run_flask)
+#     flask_thread.daemon = True
+#     flask_thread.start()
+
+#     window = webview.create_window(
+#         title='Revolutionary Invoice',
+#         url='http://127.0.0.1:5004',
+#         width=1200,
+#         height=800,
+#         resizable=True
+#     )
+
+#     window.events.closed += on_closed
+#     webview.start(gui='edgechromium')
+
 from app import app
 
-def new_func():
-    return True
-
-app.debug = new_func()
-
-def run_flask():
+if __name__ == "__main__":
     app.run(
-        host='127.0.0.1',
+        host="127.0.0.1",
         port=5004,
-        debug=False,
-        use_reloader=False
+        debug=True
     )
-
-def on_closed():
-    os._exit(0) 
-
-if __name__ == '__main__':
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.daemon = True
-    flask_thread.start()
-
-    window = webview.create_window(
-        title='Revolutionary Invoice',
-        url='http://127.0.0.1:5004',
-        width=1200,
-        height=800,
-        resizable=True
-    )
-
-    window.events.closed += on_closed
-    webview.start(gui='edgechromium')
